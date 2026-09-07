4f83a614a470ef7d436c630e8fed1cdfe31cfdd1
hyubuki <suhyuk.ts@gmail.com>
2026-09-07 15:06:36 +0900

    Chore: init repository

---
+++ src/main/java/dev/hyuki/investment_openapi/InvestmentOpenApiApplication.java
@@ -0,0 +1,13 @@
+package dev.hyuki.investment_openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentOpenApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InvestmentOpenApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/hyuki/investment_openapi/InvestmentOpenApiApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.hyuki.investment_openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentOpenApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
