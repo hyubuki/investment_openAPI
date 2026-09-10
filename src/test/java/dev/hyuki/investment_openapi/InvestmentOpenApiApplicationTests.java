43de7ccccb754d5f16d519fa3429dc9a498952ef
hyubuki <suhyuk.ts@gmail.com>
2026-09-10 09:25:18 +0900

    Chore: configure environment and add dependencies 

--- src/test/java/dev/hyuki/investment_openapi/InvestmentOpenApiApplicationTests.java
+++ src/test/java/dev/hyuki/investment_openapi/InvestmentOpenApiApplicationTests.java
@@ -2,8 +2,10 @@ package dev.hyuki.investment_openapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InvestmentOpenApiApplicationTests {
 
   @Test
